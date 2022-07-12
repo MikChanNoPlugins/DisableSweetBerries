ef0a92b7c00b18a2ec307ff9d829043140200338
wtlgo <georgeendo0@gmail.com>
2022-07-13 02:03:18 +0300

    Initial commit

---
+++ src/main/java/dev/mikchan/mcnp/disablesweetberries/DisableSweetBerriesPlugin.java
@@ -0,0 +1,17 @@
+package dev.mikchan.mcnp.disablesweetberries;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DisableSweetBerriesPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}

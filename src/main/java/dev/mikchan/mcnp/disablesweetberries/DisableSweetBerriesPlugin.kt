c53308daee0e6d7d08c51fe2cf73733c45e7b6e5
wtlgo <georgeendo0@gmail.com>
2022-07-13 04:35:00 +0300

    Add javadoc

--- src/main/java/dev/mikchan/mcnp/disablesweetberries/DisableSweetBerriesPlugin.kt
+++ src/main/java/dev/mikchan/mcnp/disablesweetberries/DisableSweetBerriesPlugin.kt
@@ -4,6 +4,11 @@ import dev.mikchan.mcnp.disablesweetberries.listeners.SweetBerriesListener
 import org.bukkit.event.HandlerList
 import org.bukkit.plugin.java.JavaPlugin
 
+/**
+ * The main plugin class.
+ *
+ * Basically, the entry point to the plugin.
+ */
 @Suppress("unused")
 class DisableSweetBerriesPlugin : JavaPlugin() {
     override fun onEnable() {
